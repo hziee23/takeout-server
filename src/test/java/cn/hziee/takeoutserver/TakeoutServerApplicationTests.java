25a863222a32eb351c0a27dd70e8b7c98a53b377
haijialiu <894226461@qq.com>
2023-10-08 01:14:38 +0800

    init

---
+++ src/test/java/cn/hziee/takeoutserver/TakeoutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hziee.takeoutserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeoutServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
